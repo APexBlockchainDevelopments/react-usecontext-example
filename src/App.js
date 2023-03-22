ae228c693ef2580a03d17c1b2936f46266ab2a8f
APex <37938428+APexBlockchainDevelopments@users.noreply.github.com>
2023-03-22 12:44:49 -0600

    new coded added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useContext, createContext, newContext } from 'react';
+
+const MyContext = createContext('initial')
 
 function App() {
+  const [value, setValue] = useState('Hello world');
+
+  const changeValue = (newValue) => {
+    setValue(newValue);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MyContext.Provider value={{ value, changeValue }}>
+      <Example1 />
+      <Example2 />
+    </MyContext.Provider>
   );
 }
 
+function Example1() {
+  const { value, changeValue } = useContext(MyContext);
+
+  return (
+    <>
+      <p>The current avlue is "{value}"</p>
+      <button onClick={() => changeValue('Example 1 Value')}>Example 1: Click me</button>
+    </>
+  )
+}
+
+function Example2() {
+  const { value, changeValue } = useContext(MyContext);
+
+  return (
+    <>
+      <p>The current avlue is "{value}"</p>
+      <button onClick={() => changeValue('Example 2 Value')}>Example 2: Click me</button>
+    </>
+  )
+}
 export default App;
